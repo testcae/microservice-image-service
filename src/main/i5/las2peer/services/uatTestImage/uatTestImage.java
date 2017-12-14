3300378a3db216be3f3602446d72a94c3672ed88
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-14 12:00:48 +0100

    Code regeneration/Model synchronization

--- src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
+++ src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
@@ -78,7 +78,7 @@ public class uatTestImage extends RESTService {
   @Api
   @SwaggerDefinition(
       info = @Info(title = "image-service", version = "",
-          description = "test",
+          description = "testaaaaa",
           termsOfService = "",
           contact = @Contact(name = "Melisa Cecilia", email = "CAEAddress@gmail.com") ,
           license = @License(name = "BSD",
