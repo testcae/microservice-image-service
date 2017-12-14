ef30a08cebeb990466aeb9f9cd872c7d42fcd85d
ukimiawz <deathmoon123>
2017-12-14 11:48:20 +0100

    Generated microservice version 1.0

---
+++ db/image-service_create_tables.sql
@@ -0,0 +1,18 @@
+--
+-- Database Schema:  uatTest
+-- Automatically generated sql script for the service image-service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tblImage.
+--
+CREATE TABLE uatTest.tblImage (
+  imageUrl VARCHAR(255) ,
+  imageId INT(11) ,
+  imageName VARCHAR(255) ,
+CONSTRAINT imageId_PK PRIMARY KEY (imageId)
+ 
+);
+
+
+
